4ab2c839e022681a3ea43c62ddb0cd7dc561bf94
Arnav Gupta <championswimmer@gmail.com>
2022-07-28 21:39:58 +0530

    generics, collectinos, and variance in Java
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- 02/java-basics/src/main/java/com/scaler/inheritence/TypeErasure.java
+++ 02/java-basics/src/main/java/com/scaler/inheritence/TypeErasure.java
@@ -9,8 +9,29 @@ public class TypeErasure {
         ArrayList<String> list = new ArrayList<>();
         list.add("asdasd");
 
-        ArrayList<Object> list2 = list;
+//        ArrayList<Object> list2 = list;
+//
+//        list2.add(123);
 
-        list2.add(123);
+        Object x = "asd";
+        System.out.println(x);
+        x = 10;
+        System.out.println(x);
+
+
+        ArrayList<Object> objList = new ArrayList<>();
+        objList.add("Asd");
+        objList.add(12);
+        objList.add(new Apple());
+
+        objList.forEach(System.out::println);
+
+        List<Fruit> fruitBasket = new ArrayList<>();
+        fruitBasket.add(new Apple());
+        fruitBasket.add(new Mango());
+
+
+        List<Apple> appleDozen = new ArrayList<Apple>(12);
+        fruitBasket.addAll(appleDozen);
     }
 }
