4388f0266d67684462e20558e04b874bce05f638
Arup Bhowmcik <arupbhowmick007@gmail.com>
2022-08-02 22:59:14 +0530

    TaskController Implementation

---
+++ 03/spring/basicapp/src/main/java/com/scaler/basicapp/pojos/Note.java
@@ -0,0 +1,27 @@
+package com.scaler.basicapp.pojos;
+
+public class Note {
+    private String title;
+    private String body;
+
+    public Note(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
