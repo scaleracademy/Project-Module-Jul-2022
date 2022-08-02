7ac1bf06af15ff7fcec78e5c15dd23fad699c928
Arup Bhowmcik <arupbhowmick007@gmail.com>
2022-08-02 22:21:07 +0530

    Committing Self-Work in separate branch.

---
+++ 03/spring/springproject/src/main/java/com/scaler/springproject/SpringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectApplication.class, args);
+	}
+
+}
