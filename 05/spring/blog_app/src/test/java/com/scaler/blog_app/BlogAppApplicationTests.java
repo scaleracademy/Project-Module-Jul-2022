e338fa08d154fe6d930f3a770d921c7644cd3636
Arnav Gupta <championswimmer@gmail.com>
2022-08-08 21:47:22 +0530

    initial project setup for blogging project
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 05/spring/blog_app/src/test/java/com/scaler/blog_app/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
